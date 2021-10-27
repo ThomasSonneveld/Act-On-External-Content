1923e8602c62d5508a265462ba23dbf5f39db271
ThomasSonneveld <49724551+ThomasSonneveld@users.noreply.github.com>
2021-10-27 09:57:46 +0200

    Add files via upload

--- content.js
+++ content.js
@@ -271,7 +271,7 @@ fetch("https://www.frankwatching.com/feed/academy/upcoming/")
   }
 
   setTimeout(function() {
-    for (var i = 0, len = 15; i < len; i++) {
+    for (var i = 0, len = 25; i < len; i++) {
       agendaItems(items[i]);
     }
 
